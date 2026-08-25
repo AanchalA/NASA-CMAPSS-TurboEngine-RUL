55fcfa27f9f580a8970e6850202a5a65fdf9c413
AanchalA <aanchaldagarwal@gmail.com>
2026-08-25 06:34:30 +0000

    Fixed errors

--- code/src/data_processing/__init__.py
+++ code/src/data_processing/__init__.py
@@ -3,11 +3,6 @@ from src.data_processing.constants import (
     CMAPSS_SENSOR_COLUMNS,
     SUPPORTED_SUBSETS,
 )
-from src.data_processing.feature_engineering import (
-    add_ewma_features,
-    add_lag_difference_features,
-    rolling_slope_and_statistics_features,
-)
 from src.data_processing.sequences import build_endpoint_sequences, build_sliding_window_sequences
 from src.data_processing.tabular import add_pandas_temporal_features, select_tabular_model_inputs
 
@@ -15,9 +10,6 @@ __all__ = [
     "CMAPSS_COLUMNS",
     "CMAPSS_SENSOR_COLUMNS",
     "SUPPORTED_SUBSETS",
-    "add_ewma_features",
-    "add_lag_difference_features",
-    "rolling_slope_and_statistics_features",
     "add_pandas_temporal_features",
     "build_endpoint_sequences",
     "build_sliding_window_sequences",
